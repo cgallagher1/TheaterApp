701b4cdbcc72cc964b106b16669e92d6e41b1774
Charles Gallagher <cgallagher1@carthage.edu>
2018-11-26 18:17:43 -0600

    Added opac photo

---
+++ signupJS.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyAh8ZnBrOq0QCn3zy0g1YrZ9bCifWHDM1M",
+    authDomain: "fir-test-d3ac0.firebaseapp.com",
+    databaseURL: "https://fir-test-d3ac0.firebaseio.com",
+    projectId: "fir-test-d3ac0",
+    storageBucket: "fir-test-d3ac0.appspot.com",
+    messagingSenderId: "1009182389821"
+  };
+
+  firebase.initializeApp(config);
