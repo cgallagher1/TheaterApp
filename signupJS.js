04bae5866ac202e279d58f0d8448a3a0ede53762
Charles Gallagher <cgallagher1@carthage.edu>
2018-11-28 17:21:27 -0600

    signup Connected to firebase

--- signupJS.js
+++ signupJS.js
@@ -1,10 +1,27 @@
 var config = {
     apiKey: "AIzaSyAh8ZnBrOq0QCn3zy0g1YrZ9bCifWHDM1M",
     authDomain: "fir-test-d3ac0.firebaseapp.com",
-    databaseURL: "https://fir-test-d3ac0.firebaseio.com",
+    databaseURL: "https://fir-test-d3ac0.firebaseio.com/",
     projectId: "fir-test-d3ac0",
     storageBucket: "fir-test-d3ac0.appspot.com",
     messagingSenderId: "1009182389821"
   };
 
   firebase.initializeApp(config);
+
+function addUser(){
+  var username = document.getElementById('usernameInput').value;
+  var password = document.getElementById('passwordInput').value;
+  console.log(username);
+  console.log(password);
+  firebase.database().ref('Username/'+ username).set({
+    Username: username,
+    Password: password
+  });
+
+  document.getElementById('usernameInput').value = "";
+  document.getElementById('passwordInput').value = "";
+  document.getElementById('confirmPasswordInput').value = "";
+
+  
+}
